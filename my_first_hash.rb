010c8aa781cd86874e2494dd8e4aa97c205691cc
Adam <luohaoxiang@yahoo.com>
2019-03-24 17:41:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def adding
                   "oil paintings" => 3
                  }
  
-shipping_manifest["muskets"] = 2, 
+shipping_manifest["muskets"] = 2 
 shipping_manifest["gun powder"] = 4
   return shipping_manifest
 end
